b332d4085a0d578726e7c583d428165f5a25d8f7
bhanuchanderdumpala <banu.tsw@gmail.com>
2025-05-21 00:43:48 +0530

    Lot of chnages

--- shoppingAdda/app.js
+++ shoppingAdda/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var validateUserCredentials = require('./routes/validateUserCredentials');
+var employeeDetails= require('./routes/getempdetails');
+
+
 
 var app = express();
 
@@ -22,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/validate/user/credentials', validateUserCredentials);
+app.use('/employee/details', employeeDetails);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
